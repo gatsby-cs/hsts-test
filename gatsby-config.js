42d3043a9582aef7a181b5a79b53a44d05c14f1e
Ben Robertson <hi@benrobertson.io>
2022-12-13 14:35:45 -0500

    Add hsts config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,15 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-gatsby-cloud',
+
+        options: {
+          allPageHeaders: [
+            "Strict-Transport-Security: max-age=31536000; includeSubDomains; preload",
+          ],
+        }
+    }
+  ],
 }
